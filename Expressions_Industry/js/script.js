c062dbad0ba4b48060aba451b0b32a1f0144715f
kalebisrael <kalebisrael@Kalebs-MacBook-Pro.local>
2014-12-05 02:34:39 -0800

    Created prompts for Industry Expressions

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,2 +1,6 @@
-// Kaleb Israel 12-4-14 Expressions Worksheet
+// Kaleb Israel 12-4-14 Expressions Industry
 
+//Upload time calculator
+var fileSize = prompt("Upload time calculator \nWhat is the file size? (MB)","0"); //prompt user for file size
+var upSpeed = prompt("What is your upload speed? (Mbps)","0"); //prompt user for upload speed
+var overhead = prompt("Overhead (%)","10"); //prompt user for overhead
