a0a0b60e2f2fa91d0f0db924af62388c3292d0ff
kalebisrael <kalebisrael@Kalebs-MacBook-Pro.local>
2014-12-05 01:59:37 -0800

    Completed Personal Calculator

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,14 +1,14 @@
 // Kaleb Israel 12-4-14 Expressions Personal
 
 //Financial Calculator
-var currentFunds = prompt("Investment Calculator \nWhat are your current funds?","1000"); //prompt user for current funds
+var funds = Number(prompt("Investment Calculator \nWhat are your current funds?","1000")); //prompt user for current funds. Number added because it was causing issues without
 var contr = prompt("What amount will you contribute monthly?","1000"); //prompt user for monthly add
 contr *= 12; //change to annual
 var interestRate = prompt("What is the interest rate percentage?","12"); //prompt user for interest rate
-interestRate = interestRate / 100 + 1; //change to decimal +1
 var invTime = prompt("How many years will you contribute?","10"); //prompt user for years estimated
-var result = currentFunds * Math.pow(interestRate,invTime) + contr * ((Math.pow(interestRate,(Number(invTime) + 1)) - interestRate) / (interestRate - 1)); //ridiculous equation that calculates compound interest once per year
+var result = funds * Math.pow((interestRate / 100 + 1),invTime) + contr * ((Math.pow((interestRate / 100 + 1),(Number(invTime) + 1)) - (interestRate / 100 + 1)) / (interestRate / 100)); //ridiculous equation that calculates compound interest once per year
+var invMade = invTime * contr + funds;
 function currencyFormat (num) { //function named currencyFormat that will return num
-    return "$" + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,") //return '$' string + num taken to two decimal places and given currency commas
+    return "$" + num.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ","); //return '$' string + num taken to two decimal places and given currency commas
 } //end fn
-console.log(currencyFormat(result)); //log result
+console.log("You currently have " + currencyFormat(funds) + ". If you contribute " + currencyFormat(contr) + " per year at " + interestRate + "% interest rate for " + invTime + " years, you will earn " + currencyFormat(result - invMade) + " in interest after investing " + currencyFormat(invMade) + " in total. I'll see you in 10 years again when you have a whopping " + currencyFormat(result) + " in total to spend!"); //log result
