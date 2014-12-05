cbba7822a187188383afb05298a6eba7c13e08f6
kalebisrael <kalebisrael@Kalebs-MacBook-Pro.local>
2014-12-04 17:25:01 -0800

    Screencast: Assignment Operators

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,12 +1,6 @@
-// Expressions - Casting
+// Expressions - Assignment Operators
 
-var stringVar = "6";
-var result = 7 + Number(stringVar);
-console.log(result);
-
-var areaCode = 541;
-var firstPart = 727;
-var secPart = 2324;
-//(541)727-2324
-var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
-console.log(phoneNo);
+var a = 3;
+a += 4; // a = a + 4
+a ++; // a += 1 // a = a + 1
+console.log(a);
