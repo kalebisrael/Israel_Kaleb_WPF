35353e37a3b09cd14318f980eef1fe2861065c57
kalebisrael <kalebisrael@Kalebs-MacBook-Pro.local>
2014-12-11 15:34:23 -0800

    Screencast: Conditional Logic

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,11 +1,10 @@
-// Nesting Conditionals
+//Conditional Logic
 
-if(sunny){
-    goToTheBeach();
+var oldEnough = false;
 
-    if(warmWater){
-        wearNewSuit();
-    }
-}else{
-    goToTheMovies();
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
 }
+console.log("what comes after");
