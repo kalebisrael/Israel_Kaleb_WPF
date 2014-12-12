467b1fecdff45ce7369895f34d7f1e182ba180f6
kalebisrael <kalebisrael@Kalebs-MacBook-Pro.local>
2014-12-11 18:53:32 -0800

    Screencast: Logical Operators

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,18 +1,12 @@
-//Conditional Logic - else if
+//Conditional Logic - Logical Operators
 
-var kidHeight = 52;
-var minHeight = 48;
-var wParentHeight = 45; //the height of the kid with the parent
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
 
-//if the child is old enough, print to the console "you can ride!"
-//if the kid is over 48 inches in height
-if(kidHeight > minHeight){
-    //you can ride!
-    console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight){
-    //you can ride with a parent present
-    console.log("You can ride, but only with a parent present.");
+//if phone price is less than budget OR wonLottery is true
+if(iPhonePrice < budget || wonLottery){
+    console.log("We can buy the phone!");
 }else{
-    //code performed if condition is false
-    console.log("Sorry kid, you've got some growing to do first!");
+    console.log("No phone for you!");
 }
