a6e7cbc77d421b131de61332b5ee764085c15ce0
kalebisrael <kalebisrael@Kalebs-MacBook-Pro.local>
2014-12-18 15:33:53 -0800

    Functions - Variable Scope

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,14 +1,14 @@
-//Functions - Function Execution
+//Functions - Variable Scope
 
-function outptMsg(){
-    console.log("Hello World");
-}
+var width = 5; //v
 
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log(area);
+    //console.log(area);
 }
+
+console.log(width); //^ scope matches width not in function
 
 calcArea();
