78e7ae51b7e8652f6542fb6b8f3bb093481b4eaa
kalebisrael <kalebisrael@Kalebs-MacBook-Pro.local>
2014-12-18 16:08:09 -0800

    Functions - Returning Values

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,17 +1,10 @@
-//Functions - Parameters and Arguments
+//Functions - Returning Values
 
-//calcArea(30, 20);
+var total = calcArea(30, 20);
 
-//function calcArea(w, h){
-//    var area = w * h;
-//    console.log(area);
-//}
-
-function dogYears(age){
-    var dogYears = age * 7;
-    console.log("Sparky is " + dogYears + "years old.");
+function calcArea(w, h){
+    var area = w * h;
+    return area;
 }
 
-var age1 = 4;
-
-dogYears(age1);
+console.log(total);
